146ef8681f4c1d3606fc65f88bd9da8a4bbce97d
Nissan Barua <baruanissan55.com>
2024-10-05 20:21:57 +0600

    category section add in js and html

--- script/video.js
+++ script/video.js
@@ -1,12 +1,21 @@
 const loadCatagory = () => {
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then((res) => res.json())
-    .then((data) => displayCatagory(data.categories))
+    .then((data) => displayCatagories(data.categories))
     .catch((err) => console.log(err));
 };
 
-const displayCatagory = (data) => {
-  console.log(data);
+const displayCatagories = (categories) => {
+  console.log(categories);
+
+  categories.forEach(element => {
+          console.log(element)
+          const categoryContainer = document.getElementById('category')
+          const button = document.createElement('button')
+          button.classList = 'btn'
+          button.innerText = element.category
+          categoryContainer.append(button)
+  });
 };
 
 loadCatagory();
